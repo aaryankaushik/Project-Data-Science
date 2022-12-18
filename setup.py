d7fde8b1c6888cbc9b45f4135cb88492d4a6acc1
Rahmanda Wibowo <rahmandawibowo@gmail.com>
2022-12-18 12:30:25 +0100

    feat: create sites to weather data csv

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="dsp",
+    version="0.1.0",
+    author="Aaryan Kaushik, Adina Asim, Mohammad Sazegar, Rahmanda Wibowo",
+    author_email="rahmanda.wibowo@student.uhasselt.be",
+    url="https://github.com/aaryankaushik/Project-Data-Science",
+    packages=["dsp"]
+)
